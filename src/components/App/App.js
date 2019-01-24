8de4bbf51f4c069b2e485ba49006180cc5995bbb
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-01-23 20:06:58 -0700

    Add functionality to fetch people from star wars api with all spec required fields

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,8 +9,61 @@ class App extends Component {
       people: [],
       planets: [],
       vehicles: [],
-      favorites: []
+      favorites: [],
+      filmToShow: ''
     }
+  }
+
+  fetchPeople = async () => {
+    let people = [];
+
+    for (let i = 1; i < 10; i++) {
+      try {
+        const response = await fetch(`https://swapi.co/api/people/?page=${i}`);
+        const result = await response.json();
+        people.push(...result.results)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    const peopleWithWorldInfo = await this.fetchWorldInfo(people);
+    const peopleWithSpeciesAndWorldInfo = await this.fetchSpeciesInfo(peopleWithWorldInfo);
+    this.setState({ people: peopleWithSpeciesAndWorldInfo});
+  }
+
+  fetchWorldInfo = (peopleArray) => {
+    const unresolvedPromises = peopleArray.map(async (person) => {
+      const response = await fetch(person.homeworld);
+      const result = await response.json();
+      return {
+        ...person,
+        homeworld: result.name,
+        population: result.population
+      }
+    });
+    return Promise.all(unresolvedPromises)
+  }
+
+  fetchSpeciesInfo = (peopleArray) => {
+    const unresolvedPromises = peopleArray.map(async (person) => {
+      if (person.species.length > 0) {
+        const response = await fetch(person.species);
+        const result = await response.json();
+        return ({ ...person, species: result.name })
+      } else {
+        return ({
+          ...person,
+          species: 'unknown'
+        })
+      }
+    });
+    return Promise.all(unresolvedPromises)
+  }
+
+  pickRandomFilm = (filmArray) => {
+    let index = Math.floor((Math.random() * filmArray.length));
+    return filmArray[index];
   }
 
   componentDidMount = async () => {
@@ -18,7 +71,9 @@ class App extends Component {
     const response = await fetch(url)
     const result = await response.json();
     const films = await result.results;
-    this.setState({films});
+    const filmToShow = await this.pickRandomFilm(films);
+    this.setState({films, filmToShow});
+    this.fetchPeople();
   }
 
   render() {
@@ -27,6 +82,7 @@ class App extends Component {
         <header className="App-header">
         <h1>SWAPI BOX</h1>
         </header>
+        <p>{this.state.filmToShow.opening_crawl}</p>
       </div>
     );
   }
