11cf046bb1078461731e60bbeebae2e1585eadae
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-01-23 17:44:20 -0700

    Add fetch for films

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      films: [],
+      people: [],
+      planets: [],
+      vehicles: [],
+      favorites: []
+    }
+  }
+
+  componentDidMount = async () => {
+    const url = 'https://swapi.co/api/films';
+    const response = await fetch(url)
+    const result = await response.json();
+    const films = await result.results;
+    this.setState({films});
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
+        <h1>SWAPI BOX</h1>
         </header>
       </div>
     );
