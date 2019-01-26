b0031abb86351b7bfee5c791f46405a261b3ae80
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-01-25 18:20:20 -0700

    Update snapshot and test for app

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -3,36 +3,31 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 
-it('should match the snapshot with all data passed in', () => {
-  const wrapper = shallow(<App />);
-  expect(wrapper).toMatchSnapshot();
-});
+  it('should match the snapshot with all data passed in', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toMatchSnapshot();
+  });
 
-it('should fetch people', () => {
+  it('should fetch people', () => {
 
-});
+  });
 
-it('should fetch world information', () => {
+  it('should fetch world information', () => {
 
-});
+  });
 
-it('should fetch species information', () => {
+  it('should fetch species information', () => {
 
-});
+  });
 
-it('should choose a random film index', () => {
-  const wrapper = shallow(<App />);
-  const mockFilms = [{ episode_number: 1 }, { episode_number: 2 }];
-  const filmIndex = wrapper.instance().getRandomFilmIndex(mockFilms);
-  expect(filmIndex).toBeLessThanOrEqual(mockFilms.length);
-});
+  it('should update category', () => {
 
-it('should update category', () => {
-
+  });
 });
